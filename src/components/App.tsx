0f446c09e189cbd944638731fd72d33bc66a5e1f
WitekG14 <witold@grzegorczyk.it>
2026-01-30 12:15:54 +0000

    Keep Track Of ActiveId (Reading And Writing To URL, UseEffect)

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Background from "./Background";
 import Container from "./Container";
 import Footer from "./Footer";
@@ -18,8 +18,22 @@ import { useJobItems } from "../lib/hooks";
 
 function App() {
   const [searchText, setSearchText] = useState("");
-
   const [jobItems, isLoading] = useJobItems(searchText);
+  const [activeId, setActiveId] = useState<number | null>(null);
+
+  useEffect(() => {
+    const handleChangeHash = () => {
+      const id = +window.location.hash.slice(1);
+      setActiveId(id);
+    };
+    handleChangeHash();
+
+    window.addEventListener("hashchange", handleChangeHash);
+
+    return () => {
+      window.removeEventListener("hashchange", handleChangeHash);
+    };
+  }, []);
 
   return (
     <>
