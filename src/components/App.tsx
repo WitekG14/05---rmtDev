64bce32a252e55003f88904c9e991dd31ea02aae
WitekG14 <witold@grzegorczyk.it>
2026-01-31 18:09:54 +0000

    Results Count (Derived State, Custom Hook)

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -18,7 +18,7 @@ import { useJobItems } from "../lib/hooks";
 
 function App() {
   const [searchText, setSearchText] = useState("");
-  const [jobItems, isLoading] = useJobItems(searchText);
+  const [jobItems, isLoading, totalNumberOfResults] = useJobItems(searchText);
 
   return (
     <>
@@ -35,7 +35,7 @@ function App() {
       <Container>
         <Sidebar>
           <SidebarTop>
-            <ResultsCount />
+            <ResultsCount totalNumberOfResults={totalNumberOfResults} />
             <Sorting />
           </SidebarTop>
 
