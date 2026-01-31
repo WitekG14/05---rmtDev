1757e72e12ef1fa47b0cc624464a05d7bf5d22e0
WitekG14 <witold@grzegorczyk.it>
2026-01-31 18:03:12 +0000

    Active Item In List (Custom Hook)

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -1,3 +1,4 @@
+import { useActiveId } from "../lib/hooks";
 import { type JobItem } from "../lib/types";
 import JobListItem from "./JobListItem";
 import Spinner from "./Spinner";
@@ -8,12 +9,18 @@ type Props = {
 };
 
 export function JobList({ jobItems, isLoading }: Props) {
+  const activeId = useActiveId();
+
   return (
     <ul className="job-list">
       {isLoading ? <Spinner /> : null}
       {!isLoading
         ? jobItems.map((jobItem) => (
-            <JobListItem key={jobItem.id} jobItem={jobItem} />
+            <JobListItem
+              key={jobItem.id}
+              jobItem={jobItem}
+              isActive={jobItem.id === activeId}
+            />
           ))
         : null}
     </ul>
