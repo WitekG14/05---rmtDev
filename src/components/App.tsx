510c4c98b25b4dfe585de408aaa3af3042006967
WitekG14 <witold@grzegorczyk.it>
2026-02-06 11:27:31 +0000

    Pagination

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -18,12 +18,24 @@ import { useDebounce, useJobItems } from "../lib/hooks";
 import { Toaster } from "react-hot-toast";
 
 function App() {
+  // state
   const [searchText, setSearchText] = useState("");
   const debouncedSearchText = useDebounce(searchText, 250);
   const { jobItems, isLoading } = useJobItems(debouncedSearchText);
+  const [currentPage, setCurrentPage] = useState(1);
 
+  // derived / computed state
   const totalNumberOfResults = jobItems?.length || 0;
   const jobItemsSliced = jobItems?.slice(0, 7) || [];
+
+  // event handlers
+  const handleChangePage = (direction: "next" | "previous") => {
+    if (direction === "next") {
+      setCurrentPage((prev) => prev + 1);
+    } else if (direction === "previous") {
+      setCurrentPage((prev) => Math.max(prev - 1, 1));
+    }
+  };
 
   return (
     <>
@@ -46,7 +58,7 @@ function App() {
 
           <JobList jobItems={jobItemsSliced} isLoading={isLoading} />
 
-          <Pagination />
+          <Pagination currentPage={currentPage} onClick={handleChangePage} />
         </Sidebar>
         <JobItemContent />
       </Container>
--- src/components/PaginationControls.tsx
+++ src/components/PaginationControls.tsx
@@ -1,3 +1,22 @@
-export default function Pagination() {
-  return <section className="pagination"></section>;
+import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons";
+
+type Props = {
+  currentPage: number;
+  onClick: (direction: "next" | "previous") => void;
+};
+
+export default function Pagination({ currentPage, onClick }: Props) {
+  return (
+    <section className="pagination">
+      <button
+        className="pagination__button"
+        onClick={() => onClick("previous")}
+      >
+        <ArrowLeftIcon /> Page {currentPage - 1}
+      </button>
+      <button className="pagination__button" onClick={() => onClick("next")}>
+        Page {currentPage + 1} <ArrowRightIcon />
+      </button>
+    </section>
+  );
 }
